a2a03070c5801b7664483b7621cae44bbedba2c5
cb-rohitganesh <rohit.ganesh@chargebee.com>
2024-09-12 10:25:50 +0530

    e2e service initialized

---
+++ src/main/java/com/springservice/springe2eservice/SpringE2eServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springservice.springe2eservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringE2eServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringE2eServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springservice/springe2eservice/SpringE2eServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springservice.springe2eservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringE2eServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
